8ba41c92e6d9848eff731a61e34a86bbd8d01005
terminato123 <100470588+terminato123@users.noreply.github.com>
2022-05-07 12:07:38 +0530

    Time: 1176 ms (48.01%), Space: 0B (100.00%) - LeetHub

---
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -0,0 +1,14 @@
+select
+  a.product_id,
+  b.product_name
+from
+  sales as a
+left join
+  product as b
+on 
+  a.product_id = b.product_id
+group by
+  a.product_id
+having
+  min(a.sale_date) >= '2019-01-01'
+  and max(a.sale_date) <= '2019-03-31' 
